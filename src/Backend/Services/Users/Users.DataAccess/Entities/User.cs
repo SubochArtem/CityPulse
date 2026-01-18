033e7726ec4627f8687f4444747d4747bd170341
artem <ghtp7397@gmail.com>
2026-01-18 16:11:18 +0300

    Add User entity and role enumeration

---
+++ src/Backend/Services/Users/Users.DataAccess/Entities/User.cs
@@ -0,0 +1,16 @@
+using Users.DataAccess.Enums;
+
+namespace Users.DataAccess.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public int? CityId { get; set; }
+    public UserRole Role { get; set; } = UserRole.User;
+    public bool IsActive { get; set; } = true;
+    public bool EmailVerified { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ src/Backend/Services/Users/Users.DataAccess/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+namespace Users.DataAccess.Enums;
+
+public enum UserRole
+{
+    User = 0,
+    Admin = 1,
+    CityManager = 2
+}
