6e556b077f9042134bce01a8c8961fd6f75529ab
artem <ghtp7397@gmail.com>
2026-01-20 06:38:35 +0300

    Updated user entity properties

--- src/Backend/Services/Users/Users.DataAccess/Entities/User.cs
+++ src/Backend/Services/Users/Users.DataAccess/Entities/User.cs
@@ -8,12 +8,11 @@ public class User
     public string Email { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
     public string Username { get; set; } = null!;
-    public int? CityId { get; set; }
     public UserRole Role { get; set; } = UserRole.User;
     public bool IsActive { get; set; } = true;
     public bool EmailVerified { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-    public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    public IEnumerable<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
     public CityManager? CityManager { get; set; }
 }
