a641946dc2da14be34c6e89b4bfdc6bccd5fdb1e
artem <ghtp7397@gmail.com>
2026-01-20 06:54:09 +0300

    Deleted cityId from cityManager entity

--- src/Backend/Services/Users/Users.DataAccess/Entities/CityManager.cs
+++ src/Backend/Services/Users/Users.DataAccess/Entities/CityManager.cs
@@ -4,6 +4,5 @@ public class CityManager
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
-    public int CityId { get; set; }
     public bool IsActive { get; set; } = true;
 }
