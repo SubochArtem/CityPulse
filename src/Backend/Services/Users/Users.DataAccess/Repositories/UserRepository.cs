c5dd10cebb9d39fb40fbc15442a6f95fabffe881
artem <ghtp7397@gmail.com>
2026-02-06 14:00:23 +0300

    use primary constructor in userRepository

--- src/Backend/Services/Users/Users.DataAccess/Repositories/UserRepository.cs
+++ src/Backend/Services/Users/Users.DataAccess/Repositories/UserRepository.cs
@@ -4,18 +4,12 @@ using Users.DataAccess.Interfaces;
 
 namespace Users.DataAccess.Repositories;
 
-public class UserRepository : Repository<User>, IUserRepository
+public class UserRepository(ApplicationDbContext context)
+    : Repository<User>(context), IUserRepository
 {
-    public UserRepository(ApplicationDbContext context)
-        : base(context)
-    {
-    }
-
-
     public async Task<User?> GetByAuth0UserIdAsync(string auth0UserId)
     {
         return await _dbSet
             .FirstOrDefaultAsync(u => u.Auth0UserId == auth0UserId);
     }
-    
 }
