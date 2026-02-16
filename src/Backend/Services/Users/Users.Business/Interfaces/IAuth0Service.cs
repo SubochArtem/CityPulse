4d41b342c9b5302db7f7a01afd4fa54629fa6416
artem <ghtp7397@gmail.com>
2026-02-16 15:20:11 +0300

    refactor(auth0): add CancellationToken to IAuth0Service methods

--- src/Backend/Services/Users/Users.Business/Interfaces/IAuth0Service.cs
+++ src/Backend/Services/Users/Users.Business/Interfaces/IAuth0Service.cs
@@ -2,9 +2,15 @@ namespace Users.Business.Interfaces;
 
 public interface IAuth0Service
 {
-    Task BlockUserAsync(string auth0UserId);
+    Task BlockUserAsync(
+        string auth0UserId,
+        CancellationToken cancellationToken = default);
 
-    Task UnblockUserAsync(string auth0UserId);
+    Task UnblockUserAsync(
+        string auth0UserId,
+        CancellationToken cancellationToken = default);
 
-    Task DeleteUserAsync(string auth0UserId);
+    Task DeleteUserAsync(
+        string auth0UserId,
+        CancellationToken cancellationToken = default);
 }
