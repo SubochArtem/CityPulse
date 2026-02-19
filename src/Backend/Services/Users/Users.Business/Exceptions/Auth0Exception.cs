5f33d8b5813ff68e054caf0adc86f5026e78d302
artem <ghtp7397@gmail.com>
2026-02-19 12:33:44 +0300

    refactor: remove HttpStatusCode from Auth0Exception

--- src/Backend/Services/Users/Users.Business/Exceptions/Auth0Exception.cs
+++ src/Backend/Services/Users/Users.Business/Exceptions/Auth0Exception.cs
@@ -1,17 +1,9 @@
-using System.Net;
-
 namespace Users.Business.Exceptions;
 
 public class Auth0Exception : ExternalServiceException
 {
-    public Auth0Exception(
-        string message,
-        HttpStatusCode? statusCode = null,
-        Exception? inner = null)
+    public Auth0Exception(string message, Exception? inner = null)
         : base(message, inner)
     {
-        StatusCode = statusCode;
     }
-
-    public HttpStatusCode? StatusCode { get; }
 }
