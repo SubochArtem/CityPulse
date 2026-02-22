d86f039ec5272fed4c7d872a9d3580fb2fbe1824
artem <ghtp7397@gmail.com>
2026-02-22 12:35:55 +0300

    feat: add IdentityProviderConstants

---
+++ src/Backend/Services/Users/Users.Business/Configurations/IdentityProviderConstants.cs
@@ -0,0 +1,11 @@
+namespace Users.Business.Configurations;
+
+public static class IdentityProviderConstants
+{
+    public const string Auth0HttpClientName = "Auth0Client";
+
+    public const string Auth0ApiV2Path = "/api/v2";
+
+    public const int TokenExpiryBufferSeconds = 300;
+
+}
