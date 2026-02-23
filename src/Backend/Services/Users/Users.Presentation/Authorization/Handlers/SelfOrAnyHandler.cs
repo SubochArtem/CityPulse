2ce292d49e380dd399ff4c1aaded56663a18ba52
artem <ghtp7397@gmail.com>
2026-02-23 13:45:53 +0300

    refactor(auth): remove DB call in SelfOrAnyHandler

--- src/Backend/Services/Users/Users.Presentation/Authorization/Handlers/SelfOrAnyHandler.cs
+++ src/Backend/Services/Users/Users.Presentation/Authorization/Handlers/SelfOrAnyHandler.cs
@@ -1,43 +1,37 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
-using Users.Business.Interfaces;
 using Users.Presentation.Authorization.Requirements;
 
 namespace Users.Presentation.Authorization.Handlers;
 
 public sealed class SelfOrAnyHandler(
-    IUserService userService,
     IHttpContextAccessor httpContextAccessor) : AuthorizationHandler<SelfOrAnyRequirement>
 {
     private const string RouteIdParameter = "id";
-    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
-    private readonly IUserService _userService = userService;
 
-    protected override async Task HandleRequirementAsync(
+    protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context,
         SelfOrAnyRequirement requirement)
     {
         if (context.User.HasClaim(Permissions.ClaimType, requirement.AnyPermission))
         {
             context.Succeed(requirement);
-            return;
+            return Task.CompletedTask;
         }
 
-        if (!context.User.HasClaim(Permissions.ClaimType, requirement.SelfPermission))
-            return;
+        var routeId = httpContextAccessor.HttpContext?.GetRouteValue(RouteIdParameter)?.ToString();
+        var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        var routeId = _httpContextAccessor.HttpContext?.GetRouteValue(RouteIdParameter)?.ToString();
-        var identityId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (!context.User.HasClaim(Permissions.ClaimType, requirement.SelfPermission)
+            || routeId is null
+            || userId is null
+            || !Guid.TryParse(routeId, out var routeGuid)
+            || !Guid.TryParse(userId, out var userGuid))
+            return Task.CompletedTask;
 
-        if (routeId is null || identityId is null)
-            return;
-
-        if (!Guid.TryParse(routeId, out var routeGuid))
-            return;
-
-        var currentUser = await _userService.GetUserByAuth0IdAsync(identityId);
-
-        if (currentUser is not null && currentUser.Id == routeGuid)
+        if (userGuid == routeGuid)
             context.Succeed(requirement);
+
+        return Task.CompletedTask;
     }
 }
