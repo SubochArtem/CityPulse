d97b0d25ebd71a0922ef2be8d184ff9cd1a94f5c
artem <ghtp7397@gmail.com>
2026-02-26 12:18:20 +0300

    feature(exception-handler): add constants for problem messages and titles

---
+++ src/Backend/Services/Users/Users.Presentation/Constants/ProblemMessages.cs
@@ -0,0 +1,9 @@
+namespace Users.Presentation.Constants;
+
+public static class ProblemMessages
+{
+    public const string UnexpectedError = "An unexpected error occurred.";
+
+    public const string IdentityProviderCommunicationError =
+        "An error occurred while communicating with the identity provider.";
+}
