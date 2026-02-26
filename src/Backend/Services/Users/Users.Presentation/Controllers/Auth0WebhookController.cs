d777b93a2b9020bde854dede0f348c4d7e0d3303
artem <ghtp7397@gmail.com>
2026-02-26 10:47:18 +0300

    feature:(webhook-controller): add Auth0WebhookController

---
+++ src/Backend/Services/Users/Users.Presentation/Controllers/Auth0WebhookController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Users.Business.Configurations;
+using Users.Business.Interfaces;
+using Users.Presentation.Extensions;
+
+namespace Users.Presentation.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class Auth0WebhookController(IIdentityProviderWebhookService webhookService) : ControllerBase
+{
+    private readonly IIdentityProviderWebhookService _webhookService = webhookService;
+
+    [HttpPost]
+    public async Task<ActionResult> HandleAsync(CancellationToken cancellationToken)
+    {
+        var rawBody = await HttpContext.Request.ReadRawBodyAsync();
+        var signature = HttpContext
+            .Request
+            .Headers[IdentityProviderConstants.WebhookSignatureHeader]
+            .ToString();
+
+        await _webhookService.HandleAsync(rawBody, signature, cancellationToken);
+        return Ok();
+    }
+}
