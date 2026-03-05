3316f776f58e163e0e5b358ad9aa197bdd9a0128
artem <ghtp7397@gmail.com>
2026-03-05 17:51:08 +0300

    feat(domain): remove error code from result pattern

--- src/Backend/Services/Polls/Polls.Domain/Common/Error.cs
+++ src/Backend/Services/Polls/Polls.Domain/Common/Error.cs
@@ -2,17 +2,17 @@ using Polls.Domain.Common.Enums;
 
 namespace Polls.Domain.Common;
 
-public record Error(string Code, string Description, ErrorType Type)
+public record Error(string Description, ErrorType Type)
 {
-    public static Error NotFound(string code, string description) =>
-        new(code, description, ErrorType.NotFound);
+    public static Error NotFound(string description) =>
+        new(description, ErrorType.NotFound);
 
-    public static Error Validation(string code, string description) =>
-        new(code, description, ErrorType.Validation);
+    public static Error Validation(string description) =>
+        new(description, ErrorType.Validation);
 
-    public static Error Conflict(string code, string description) =>
-        new(code, description, ErrorType.Conflict);
+    public static Error Conflict(string description) =>
+        new(description, ErrorType.Conflict);
 
-    public static Error Failure(string code, string description) =>
-        new(code, description, ErrorType.Failure);
+    public static Error Failure(string description) =>
+        new(description, ErrorType.Failure);
 }
