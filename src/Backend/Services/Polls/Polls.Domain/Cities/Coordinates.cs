0362c8b5cba7550680b8a42be8962d78c258eca2
artem <ghtp7397@gmail.com>
2026-03-06 16:13:48 +0300

    refactor(domain): remove coordinates validation from domain layer

--- src/Backend/Services/Polls/Polls.Domain/Cities/Coordinates.cs
+++ src/Backend/Services/Polls/Polls.Domain/Cities/Coordinates.cs
@@ -1,26 +1,3 @@
-using Polls.Domain.Common;
-
 namespace Polls.Domain.Cities;
 
-public record Coordinates
-{
-    private Coordinates(double latitude, double longitude)
-    {
-        Latitude = latitude;
-        Longitude = longitude;
-    }
-
-    public double Latitude { get; }
-    public double Longitude { get; }
-
-    public static Result<Coordinates> Create(double latitude, double longitude)
-    {
-        if (latitude < -90 || latitude > 90)
-            return CityErrors.InvalidLatitude;
-
-        if (longitude < -180 || longitude > 180)
-            return CityErrors.InvalidLongitude;
-
-        return new Coordinates(latitude, longitude);
-    }
-}
+public record Coordinates(double Latitude, double Longitude);
