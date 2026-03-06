26fdeed7ed051c6080bf2f17e4f3a44db45716f8
artem <ghtp7397@gmail.com>
2026-03-06 16:33:27 +0300

    refactor(domain): remove unused errors

--- src/Backend/Services/Polls/Polls.Domain/Ideas/IdeaErrors.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/IdeaErrors.cs
@@ -5,6 +5,4 @@ namespace Polls.Domain.Ideas;
 public static class IdeaErrors
 {
     public static Error NotFound => Error.NotFound("Idea not found");
-    public static Error AlreadyInPoll => Error.Conflict("Idea is already in poll");
-    public static Error NotPending => Error.Conflict("Idea is not in pending status");
 }
