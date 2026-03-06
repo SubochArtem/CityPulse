b712574e64a99eaa6cd50e5b0b10399938a69bba
artem <ghtp7397@gmail.com>
2026-03-06 16:42:49 +0300

    feat(domain): add title, description fields and errors to idea entity

--- src/Backend/Services/Polls/Polls.Domain/Ideas/Idea.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/Idea.cs
@@ -8,4 +8,6 @@ public class Idea : EntityBase
     public Guid UserId { get; set; }
     public Guid PollId { get; set; }
     public IdeaStatus Status { get; set; } = IdeaStatus.InPoll;
+    public required string Title { get; set; }
+    public string Description { get; set; } =  string.Empty;
 }
--- src/Backend/Services/Polls/Polls.Domain/Ideas/IdeaErrors.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/IdeaErrors.cs
@@ -5,4 +5,6 @@ namespace Polls.Domain.Ideas;
 public static class IdeaErrors
 {
     public static Error NotFound => Error.NotFound("Idea not found");
+    public static Error PollAlreadyFinished => Error.Conflict("Cannot create idea in finished poll");
+    public static Error AlreadyApproved => Error.Conflict("Cannot modify approved idea");
 }
