e458edbc628deb9938c5136a81beb09fe99e7fb1
artem <ghtp7397@gmail.com>
2026-03-09 17:56:37 +0300

    feat(infrastructure): add DependencyInjection

---
+++ src/Backend/Services/Polls/Polls.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polls.Application.Common.Interfaces;
+using Polls.Infrastructure.Persistence;
+using Polls.Infrastructure.Persistence.Interceptors;
+using Polls.Infrastructure.Persistence.Repositories;
+
+namespace Polls.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services
+            .AddScoped<SaveChangesInterceptor>()
+            .AddScoped<AuditInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            var updateTimestampsInterceptor =
+                serviceProvider.GetRequiredService<SaveChangesInterceptor>();
+            var auditInterceptor =
+                serviceProvider.GetRequiredService<AuditInterceptor>();
+
+            options
+                .UseNpgsql(connectionString, npgsqlOptions =>
+                {
+                    npgsqlOptions.EnableRetryOnFailure(
+                        5,
+                        TimeSpan.FromSeconds(30),
+                        null);
+                })
+                .AddInterceptors(
+                    updateTimestampsInterceptor,
+                    auditInterceptor);
+        });
+
+        services
+            .AddScoped<ICityRepository, CityRepository>()
+            .AddScoped<IPollRepository, PollRepository>()
+            .AddScoped<IIdeaRepository, IdeaRepository>()
+            .AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
