e770ab715720dd00870cd88dda7ee775728fe6df
artem <ghtp7397@gmail.com>
2026-03-09 13:01:19 +0300

    feat(persistence): add CityConfiguration for EF Core

---
+++ src/Backend/Services/Polls/Polls.Infrastructure/Persistence/Configurations/CityConfiguration.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Polls.Domain.Cities;
+
+namespace Polls.Infrastructure.Persistence.Configurations;
+
+public class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(u => u.Id)
+            .HasColumnName("id")
+            .HasDefaultValueSql("gen_random_uuid()")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(c => c.Name)
+            .HasColumnName("name")
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(c => c.Description)
+            .HasColumnName("description")
+            .HasMaxLength(500);
+
+        builder.OwnsOne(c => c.Coordinates, coords =>
+        {
+            coords.Property(c => c.Latitude)
+                .HasColumnName("latitude")
+                .IsRequired();
+
+            coords.Property(c => c.Longitude)
+                .HasColumnName("longitude")
+                .IsRequired();
+        });
+
+        builder.Property(c => c.Status)
+            .HasColumnName("status")
+            .HasConversion<string>()
+            .IsRequired();
+
+        builder.Property(c => c.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.Property(c => c.UpdatedAt)
+            .HasColumnName("updated_at")
+            .IsRequired();
+    }
+}
