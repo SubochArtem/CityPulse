b8c184e8d6b93048346efbe78006368e1d312e51
Artem <ghtp7397@gmail.com>
2026-03-11 16:15:32 +0300

    refactor(domain): remove unnecessary error types

--- src/Backend/Services/Polls/Polls.Domain/Common/Enums/ErrorType.cs
+++ src/Backend/Services/Polls/Polls.Domain/Common/Enums/ErrorType.cs
@@ -4,7 +4,5 @@ public enum ErrorType
 {
     Unknown = 0,
     NotFound = 1,
-    Validation = 2,
-    Conflict = 3,
-    Failure = 4
+    Conflict = 2
 }
--- src/Backend/Services/Polls/Polls.Domain/Common/Error.cs
+++ src/Backend/Services/Polls/Polls.Domain/Common/Error.cs
@@ -6,13 +6,7 @@ public record Error(string Description, ErrorType Type)
 {
     public static Error NotFound(string description) =>
         new(description, ErrorType.NotFound);
-
-    public static Error Validation(string description) =>
-        new(description, ErrorType.Validation);
-
+    
     public static Error Conflict(string description) =>
         new(description, ErrorType.Conflict);
-
-    public static Error Failure(string description) =>
-        new(description, ErrorType.Failure);
 }
