93efafb6b0db26b44a8b8a2e431bd16a056c37ce
Artem <ghtp7397@gmail.com>
2026-03-11 15:30:00 +0300

    fix(exceptionhandler): delete unnessary files

--- src/Backend/Services/Users/Users.Presentation/Constants/ProblemMessages.cs
+++
@@ -1,9 +0,0 @@
-namespace Users.Presentation.Constants;
-
-public static class ProblemMessages
-{
-    public const string UnexpectedError = "An unexpected error occurred.";
-
-    public const string IdentityProviderCommunicationError =
-        "An error occurred while communicating with the identity provider.";
-}
--- src/Backend/Services/Users/Users.Presentation/Extensions/ExceptionHandlerExtensions.cs
+++ src/Backend/Services/Users/Users.Presentation/Extensions/ExceptionHandlerExtensions.cs
@@ -4,7 +4,7 @@ namespace Users.Presentation.Extensions;
 
 public static class ExceptionHandlerExtensions
 {
-    public static IApplicationBuilder UseExceptionHandler(this IApplicationBuilder builder)
+    public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<ExceptionHandlerMiddleware>();
     }
