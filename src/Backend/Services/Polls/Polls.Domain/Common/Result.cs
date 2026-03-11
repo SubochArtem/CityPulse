f1c84b6220c57e1b64187cec2bcebae170ee73db
Artem <ghtp7397@gmail.com>
2026-03-11 15:57:02 +0300

    refactor(domain): add base Result class

--- src/Backend/Services/Polls/Polls.Domain/Common/Result.cs
+++ src/Backend/Services/Polls/Polls.Domain/Common/Result.cs
@@ -1,33 +1,67 @@
 namespace Polls.Domain.Common;
 
-public class Result<T>
+public abstract class Result
 {
-    public T? Value { get; }
-    public Error? Error { get; }
-    public bool IsSuccess { get; }
-
-    private Result(T value)
+    protected Result()
     {
-        Value = value;
         IsSuccess = true;
     }
 
-    private Result(Error error)
+    protected Result(IReadOnlyList<Error> errors)
     {
-        Error = error;
+        Errors = errors;
         IsSuccess = false;
     }
 
-    public static Result<T> Success(T value) => new(value);
-    public static Result<T> Failure(Error error) => new(error);
+    public IReadOnlyList<Error> Errors { get; } = [];
+    public bool IsSuccess { get; }
+}
 
-    public static implicit operator Result<T>(T value) => Success(value);
-    public static implicit operator Result<T>(Error error) => Failure(error);
+public class Result<T> : Result
+{
+    private Result(T value)
+    {
+        Value = value;
+    }
+
+    private Result(IReadOnlyList<Error> errors) : base(errors){}
+
+    public T? Value { get; }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(value);
+    }
+
+    public static Result<T> Failure(Error error)
+    {
+        return new Result<T>([error]);
+    }
+
+    public static Result<T> Failure(IReadOnlyList<Error> errors)
+    {
+        return new Result<T>(errors);
+    }
+
+    public static implicit operator Result<T>(T value)
+    {
+        return Success(value);
+    }
+
+    public static implicit operator Result<T>(Error error)
+    {
+        return Failure(error);
+    }
+
+    public static implicit operator Result<T>(List<Error> errors)
+    {
+        return Failure(errors);
+    }
 
     public TResult Match<TResult>(
         Func<T, TResult> onSuccess,
-        Func<Error, TResult> onError)
+        Func<IReadOnlyList<Error>, TResult> onError)
     {
-        return IsSuccess ? onSuccess(Value!) : onError(Error!);
+        return IsSuccess ? onSuccess(Value!) : onError(Errors);
     }
 }
