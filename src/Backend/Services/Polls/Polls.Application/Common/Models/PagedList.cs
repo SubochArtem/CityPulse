58dd666f5e47354f0ef8c9658d47a8516e8b1193
Artem <ghtp7397@gmail.com>
2026-03-13 15:51:28 +0300

    feat(infrastructure): add PagedList and PagedListExtensions

---
+++ src/Backend/Services/Polls/Polls.Application/Common/Models/PagedList.cs
@@ -0,0 +1,11 @@
+namespace Polls.Application.Common.Models;
+
+public class PagedList<T>(List<T> items, int page, int pageSize, int totalCount)
+{
+    public List<T> Items { get; } = items;
+    public int Page { get; } = page;
+    public int PageSize { get; } = pageSize;
+    public int TotalCount { get; } = totalCount;
+    public bool HasNextPage => Page * PageSize < TotalCount;
+    public bool HasPreviousPage => Page > 1;
+}
