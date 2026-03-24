87d5d090308d5e4ab5410ee8b2b2e34c5a672e67
Artem <ghtp7397@gmail.com>
2026-03-24 18:59:42 +0300

    refactor(application): update formating in DeleteCityCommandHandler

--- src/Backend/Services/Polls/Polls.Application/Cities/Commands/DeleteCity/DeleteCityCommandHandler.cs
+++ src/Backend/Services/Polls/Polls.Application/Cities/Commands/DeleteCity/DeleteCityCommandHandler.cs
@@ -8,15 +8,15 @@ namespace Polls.Application.Cities.Commands.DeleteCity;
 public sealed class DeleteCityCommandHandler(IUnitOfWork unitOfWork)
     : IRequestHandler<DeleteCityCommand, Result<Unit>>
 {
-    public async Task<Result<Unit>> Handle(DeleteCityCommand command, CancellationToken ct)
+    public async Task<Result<Unit>> Handle(DeleteCityCommand command, CancellationToken cancellationToken)
     {
-        var city = await unitOfWork.Cities.GetByIdAsync(command.Id, ct);
+        var city = await unitOfWork.Cities.GetByIdAsync(command.Id, cancellationToken);
 
         if (city is null)
             return CityErrors.NotFound(command.Id);
 
         unitOfWork.Cities.Delete(city);
-        await unitOfWork.SaveChangesAsync(ct);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Result<Unit>.Success(Unit.Value);
     }
