2abdbd130a651df894aafed870fa1c344344eafb
Artem <ghtp7397@gmail.com>
2026-04-02 10:04:06 +0300

    feat(application): add status-based filtering for admin and public access for city entity

--- src/Backend/Services/Polls/Polls.Application/Cities/Queries/GetCities/GetCitiesQuery.cs
+++ src/Backend/Services/Polls/Polls.Application/Cities/Queries/GetCities/GetCitiesQuery.cs
@@ -4,4 +4,4 @@ using Polls.Application.Common.Models;
 
 namespace Polls.Application.Cities.Queries.GetCities;
 
-public record GetCitiesQuery(CityFilter Filter) : IQuery<PagedList<CityDto>>;
+public record GetCitiesQuery(CityFilter Filter, bool IncludeOnlyActive = true) : IQuery<PagedList<CityDto>>;
--- src/Backend/Services/Polls/Polls.Application/Cities/Queries/GetCityById/GetCityByIdQueryHandler.cs
+++ src/Backend/Services/Polls/Polls.Application/Cities/Queries/GetCityById/GetCityByIdQueryHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Polls.Application.Cities.DTOs;
 using Polls.Application.Common.Interfaces;
 using Polls.Domain.Cities;
+using Polls.Domain.Cities.Enums;
 using Polls.Domain.Common;
 
 namespace Polls.Application.Cities.Queries.GetCityById;
@@ -17,7 +18,7 @@ public sealed class GetCityByIdQueryHandler(
         CancellationToken cancellationToken)
     {
         var city = await unitOfWork.Cities.GetByIdAsync(query.Id, cancellationToken);
-        if (city is null)
+        if (city is null || (city.Status != CityStatus.Active && query.IncludeOnlyActive))
             return CityErrors.NotFound(query.Id);
 
         return mapper.Map<CityDto>(city);
