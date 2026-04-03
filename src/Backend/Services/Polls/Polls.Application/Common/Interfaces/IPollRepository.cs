ce177b36fbff30b3c72be54baeaa3fb2871cb44f
Artem <ghtp7397@gmail.com>
2026-04-03 11:25:32 +0300

    refactor(application): update formating in IPollRepository

--- src/Backend/Services/Polls/Polls.Application/Common/Interfaces/IPollRepository.cs
+++ src/Backend/Services/Polls/Polls.Application/Common/Interfaces/IPollRepository.cs
@@ -22,11 +22,5 @@ public interface IPollRepository : IRepository<Poll>
         PollStatus target,
         DateTimeOffset updatedAt,
         CancellationToken cancellationToken = default);
-
-    Task UpdateStatusByCityAsync(
-        Guid cityId,
-        PollStatus source,
-        PollStatus target,
-        DateTimeOffset updatedAt,
-        CancellationToken cancellationToken = default);
+    
 }
