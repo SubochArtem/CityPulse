738ed891205239ff8e348f6913cebe7443f5d19c
Artem <ghtp7397@gmail.com>
2026-04-08 17:07:52 +0300

    fix(application): use ICommand instead of IRequest<Result<Unit>> in ChangeIdeaStatusCommand

--- src/Backend/Services/Polls/Polls.Application/Ideas/Commands/ChangeStatus/ChangeIdeaStatusCommand.cs
+++ src/Backend/Services/Polls/Polls.Application/Ideas/Commands/ChangeStatus/ChangeIdeaStatusCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Polls.Application.Common.CQRS;
 using Polls.Domain.Common;
 using Polls.Domain.Ideas.Enums;
 
@@ -6,4 +7,4 @@ namespace Polls.Application.Ideas.Commands.ChangeStatus;
 
 public sealed record ChangeIdeaStatusCommand(
     Guid Id, 
-    IdeaStatus NewStatus) : IRequest<Result<Unit>>;
+    IdeaStatus NewStatus) : ICommand;
