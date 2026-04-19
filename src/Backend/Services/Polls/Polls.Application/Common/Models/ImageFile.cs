854674b8bb8130df92613bd74dde3a2ff654abf1
Artem <ghtp7397@gmail.com>
2026-04-19 15:45:10 +0300

    feat(infrastructure): add MinioStorageService

---
+++ src/Backend/Services/Polls/Polls.Application/Common/Models/ImageFile.cs
@@ -0,0 +1,8 @@
+namespace Polls.Application.Common.Models;
+
+public class ImageFile
+{
+    public required Stream Content;
+    public required string FileName;
+    public required string ContentType;
+}
