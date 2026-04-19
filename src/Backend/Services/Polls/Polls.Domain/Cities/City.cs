606a7d466618f5e9269ff10d1570776598ae749f
Artem <ghtp7397@gmail.com>
2026-04-19 15:31:55 +0300

    feat(infrastructure): add Image configurations

--- src/Backend/Services/Polls/Polls.Domain/Cities/City.cs
+++ src/Backend/Services/Polls/Polls.Domain/Cities/City.cs
@@ -1,5 +1,6 @@
 using Polls.Domain.Cities.Enums;
 using Polls.Domain.Common;
+using Polls.Domain.Images;
 using Polls.Domain.Polls;
 
 namespace Polls.Domain.Cities;
@@ -9,4 +10,5 @@ public class City : EntityBase
     public required Coordinates Coordinates { get; set; }
     public CityStatus Status { get; set; } = CityStatus.Undefined;
     public ICollection<Poll> Polls { get; set; } = [];
+    public ICollection<CityImage> Images { get; set; } = [];
 }
