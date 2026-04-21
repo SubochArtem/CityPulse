01cd17ab00ed7c617484fad09411c20f1ef3b7d1
Artem <ghtp7397@gmail.com>
2026-04-21 11:34:03 +0300

    refactor(application):replace public fields with properties in ImageFile

--- src/Backend/Services/Polls/Polls.Application/Common/Models/ImageFile.cs
+++ src/Backend/Services/Polls/Polls.Application/Common/Models/ImageFile.cs
@@ -2,7 +2,7 @@ namespace Polls.Application.Common.Models;
 
 public class ImageFile
 {
-    public required Stream Content;
-    public required string FileName;
-    public required string ContentType;
+    public required Stream Content { get; set; }
+    public required string FileName { get; set; }
+    public required string ContentType  { get; set; }
 }
