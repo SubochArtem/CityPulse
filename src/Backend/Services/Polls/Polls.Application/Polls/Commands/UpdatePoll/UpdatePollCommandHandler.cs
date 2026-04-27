e60985de17bf85d74d6a5228095ef80d35341990
Artem <ghtp7397@gmail.com>
2026-04-27 11:29:47 +0300

    refactor(application): move endsAtChanged declaration closer to EndsAt update in UpdatePollCommandHandler

--- src/Backend/Services/Polls/Polls.Application/Polls/Commands/UpdatePoll/UpdatePollCommandHandler.cs
+++ src/Backend/Services/Polls/Polls.Application/Polls/Commands/UpdatePoll/UpdatePollCommandHandler.cs
@@ -45,11 +45,10 @@ public sealed class UpdatePollCommandHandler(
             if (!guardResult.IsSuccess)
                 return guardResult.Error;
         }
-
-        var endsAtChanged = poll.EndsAt != command.EndsAt; 
-
+        
         poll.Title = command.Title;
         poll.Description = command.Description;
+        var endsAtChanged = poll.EndsAt != command.EndsAt;
         poll.EndsAt = command.EndsAt;
         poll.BudgetAmount = command.BudgetAmount;
 
