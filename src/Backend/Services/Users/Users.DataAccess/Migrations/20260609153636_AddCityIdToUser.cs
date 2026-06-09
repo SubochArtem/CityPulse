612dfc551ddd861d8529feb45f42b1089db15a3c
Artem <ghtp7397@gmail.com>
2026-06-09 19:07:26 +0300

    chore(db): add cityId to user entity migration

---
+++ src/Backend/Services/Users/Users.DataAccess/Migrations/20260609153636_AddCityIdToUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCityIdToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CityId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "Users");
+        }
+    }
+}
