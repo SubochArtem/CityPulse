8fce5acc47ca8a0fc311f4ac698c2eb0b819328d
Artem <ghtp7397@gmail.com>
2026-06-10 11:22:57 +0300

    feat(infrastructure): add GetFilteredAsync method to UserRepository

--- src/Backend/Services/Users/Users.DataAccess/Interfaces/IUserRepository.cs
+++ src/Backend/Services/Users/Users.DataAccess/Interfaces/IUserRepository.cs
@@ -1,8 +1,15 @@
 using Users.DataAccess.Entities;
+using Users.DataAccess.Models;
 
 namespace Users.DataAccess.Interfaces;
 
 public interface IUserRepository : IRepository<User>
 {
-    Task<User?> GetByIdentityIdAsync(string identityId, CancellationToken cancellationToken = default);
+    Task<User?> GetByIdentityIdAsync(
+        string identityId, 
+        CancellationToken cancellationToken = default);
+    
+    Task<PagedList<User>> GetFilteredAsync(
+        UserFilter filter,
+        CancellationToken cancellationToken = default);
 }
