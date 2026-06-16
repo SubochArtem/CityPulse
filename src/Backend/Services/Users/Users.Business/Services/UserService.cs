784ce6c34cb77c243cafc20ca713c8dcec916750
Artem <ghtp7397@gmail.com>
2026-06-16 15:44:20 +0300

    Revert "feat(grpc): remove exception handling from CityGrpcService"
    This reverts commit 36d9f6cba6adc1f532e8c18e7148381d5bbf76e2.

--- src/Backend/Services/Users/Users.Business/Services/UserService.cs
+++ src/Backend/Services/Users/Users.Business/Services/UserService.cs
@@ -61,6 +61,9 @@ public class UserService(
                 updateUserProfileDto.CityId.Value,
                 cancellationToken);
 
+            if (city is null)
+                throw new CityNotFoundException(updateUserProfileDto.CityId.Value);
+
             if (city.Status != CityStatus.Active)
                 throw new CityNotActiveException(updateUserProfileDto.CityId.Value);
 
@@ -73,7 +76,7 @@ public class UserService(
             user.Nickname = updateUserProfileDto.Nickname;
             isAuth0UpdateRequired = true;
         }
-    
+        
         if (isAuth0UpdateRequired)
         {
             await identityProvider.UpdateUserProfileAsync(
