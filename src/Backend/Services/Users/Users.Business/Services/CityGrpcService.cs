a87ce4d0c04a82add9a4bb53d3b800558264f6a0
Artem <ghtp7397@gmail.com>
2026-06-16 15:44:20 +0300

    Revert "feat(grpc): remove exception handling from CityGrpcService"
    This reverts commit 36d9f6cba6adc1f532e8c18e7148381d5bbf76e2.

--- src/Backend/Services/Users/Users.Business/Services/CityGrpcService.cs
+++ src/Backend/Services/Users/Users.Business/Services/CityGrpcService.cs
@@ -1,4 +1,5 @@
 using CityPulse.Contracts.Grpc.Protos;
+using Grpc.Core;
 using Users.Business.DTOs;
 using Users.Business.Interfaces;
 
@@ -6,19 +7,28 @@ namespace Users.Business.Services;
 
 public class CityGrpcService(CitiesService.CitiesServiceClient client) : ICityService
 {
-    public async Task<CityDto> GetCityAsync(
+    private readonly CitiesService.CitiesServiceClient _client = client;
+
+    public async Task<CityDto?> GetCityAsync(
         Guid cityId,
         CancellationToken cancellationToken = default)
     {
-        var response = await client.GetCityAsync(
-            new GetCityRequest { CityId = cityId.ToString() },
-            cancellationToken: cancellationToken);
-
-        return new CityDto
+        try
         {
-            Id = Guid.Parse(response.Id),
-            Name = response.Name,
-            Status = (CityStatus)response.Status
-        };
+            var response = await _client.GetCityAsync(
+                new GetCityRequest { CityId = cityId.ToString() },
+                cancellationToken: cancellationToken);
+
+            return new CityDto
+            {
+                Id = Guid.Parse(response.Id),
+                Name = response.Name,
+                Status = (CityStatus)response.Status
+            };
+        }
+        catch (RpcException ex) when (ex.StatusCode == StatusCode.NotFound)
+        {
+            return null;
+        }
     }
 }
