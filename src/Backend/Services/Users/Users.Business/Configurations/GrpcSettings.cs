1e3f96eb8163d79cdd30ebee94ac2c44de90385c
Artem <ghtp7397@gmail.com>
2026-06-18 07:47:49 +0300

    feat(grpc): move CityGrpcService from BLL to DAL

--- src/Backend/Services/Users/Users.Business/Configurations/GrpcSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Users.Business.Configurations;
-
-public class GrpcSettings
-{
-    public const string SectionName = "GrpcSettings";
-    public required string CitiesServiceUrl { get; init; }
-}
--- src/Backend/Services/Users/Users.Business/Services/UserService.cs
+++ src/Backend/Services/Users/Users.Business/Services/UserService.cs
@@ -5,6 +5,7 @@ using Users.Business.Constants;
 using Users.Business.DTOs;
 using Users.Business.Exceptions;
 using Users.Business.Interfaces;
+using Users.DataAccess.DTOs;
 using Users.DataAccess.Entities;
 using Users.DataAccess.Interfaces;
 using Users.DataAccess.Models;
