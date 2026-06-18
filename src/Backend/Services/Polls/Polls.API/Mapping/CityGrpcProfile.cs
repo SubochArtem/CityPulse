98914a3759986390bc82772b4dcdd1028fa0e539
Artem <ghtp7397@gmail.com>
2026-06-18 09:17:34 +0300

    feat(grpc): use autoMapper in CityGrpcService

---
+++ src/Backend/Services/Polls/Polls.API/Mapping/CityGrpcProfile.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using CityPulse.Contracts.Grpc.Protos;
+using Polls.Application.Cities.DTOs;
+using DomainCityStatus = Polls.Domain.Cities.Enums.CityStatus;
+using GrpcCityStatus = CityPulse.Contracts.Grpc.Protos.CityStatus;
+
+namespace Polls.API.Mapping;
+
+public class CityGrpcProfile : Profile
+{
+    public CityGrpcProfile()
+    {
+        CreateMap<DomainCityStatus, GrpcCityStatus>()
+            .ConvertUsing((status, _, _) => MapStatus(status));
+
+        CreateMap<CityDto, GetCityResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.ToString()))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Title));
+    }
+
+    private static GrpcCityStatus MapStatus(DomainCityStatus status)
+    {
+        return status switch
+        {
+            DomainCityStatus.Undefined => GrpcCityStatus.Undefined,
+            DomainCityStatus.Active => GrpcCityStatus.Active,
+            DomainCityStatus.Inactive => GrpcCityStatus.Inactive,
+            _ => throw new ArgumentOutOfRangeException(nameof(status), status, null)
+        };
+    }
+}
