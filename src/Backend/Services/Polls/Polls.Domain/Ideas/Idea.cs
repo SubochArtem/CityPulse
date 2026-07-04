061c56762b38d18f7391c31aa078235239a651d4
Artem <ghtp7397@gmail.com>
2026-07-04 16:01:52 +0300

    feat(domain): add ApprovalStatus and AccessStatus properties to IdeaEntity

--- src/Backend/Services/Polls/Polls.Domain/Ideas/Idea.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/Idea.cs
@@ -10,6 +10,7 @@ public class Idea : EntityBase
     public Guid UserId { get; set; }
     public Guid PollId { get; set; }
     public Poll Poll { get; set; } = null!;
-    public IdeaStatus Status { get; set; } = IdeaStatus.Undefined;
+    public AccessStatus AccessStatus { get; set; } = AccessStatus.Undefined;
+    public ApprovalStatus ApprovalStatus { get; set; } = ApprovalStatus.Undefined;
     public ICollection<IdeaImage> Images { get; set; } = [];
 }
