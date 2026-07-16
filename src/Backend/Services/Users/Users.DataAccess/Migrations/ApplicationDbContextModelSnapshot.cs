205d97eb1f54c213849a9d57e070a6a49aa7e1c4
Artem <ghtp7397@gmail.com>
2026-07-16 10:08:47 +0300

    chore(database): create AddUserAccessStatus migration

--- src/Backend/Services/Users/Users.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Backend/Services/Users/Users.DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Users.DataAccess.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.29")
+                .HasAnnotation("ProductVersion", "8.0.28")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
