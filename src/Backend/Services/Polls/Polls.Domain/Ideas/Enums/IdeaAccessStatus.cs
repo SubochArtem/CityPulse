4d0446ae3090603cc8e8a07240c7797cd2fdaec9
Artem <ghtp7397@gmail.com>
2026-07-20 21:07:05 +0300

    feat(domain): add RestrictedByContext and RestrictedByAuthor properties to IdeaAccessStatus enum

--- src/Backend/Services/Polls/Polls.Domain/Ideas/Enums/IdeaAccessStatus.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/Enums/IdeaAccessStatus.cs
@@ -5,5 +5,7 @@ public enum IdeaAccessStatus
     Undefined = 0,
     Active = 1,
     Inactive = 2,
-    Restricted = 3
+    Restricted = 3,
+    RestrictedByContext = 4,
+    RestrictedByAuthor = 5
 }
