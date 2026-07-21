ecd993d77e4029c7804ef3126190028a76b86b98
Artem <ghtp7397@gmail.com>
2026-07-21 13:51:37 +0300

    feat(domain): remove Restricted property from IdeaAccessStatus enum

--- src/Backend/Services/Polls/Polls.Domain/Ideas/Enums/IdeaAccessStatus.cs
+++ src/Backend/Services/Polls/Polls.Domain/Ideas/Enums/IdeaAccessStatus.cs
@@ -5,7 +5,6 @@ public enum IdeaAccessStatus
     Undefined = 0,
     Active = 1,
     Inactive = 2,
-    Restricted = 3,
-    RestrictedByContext = 4,
-    RestrictedByAuthor = 5
+    RestrictedByContext = 3,
+    RestrictedByAuthor = 4
 }
