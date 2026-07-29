fe5eb3babe953b73acfe4bda9b353a41e3b84a4d
Artem <ghtp7397@gmail.com>
2026-07-29 11:20:45 +0300

    feat(business): add Nickname and CityId properties to GetUserDto

--- src/Backend/Services/Users/Users.Business/DTOs/GetUserDto.cs
+++ src/Backend/Services/Users/Users.Business/DTOs/GetUserDto.cs
@@ -3,6 +3,8 @@ namespace Users.Business.DTOs;
 public class GetUserDto
 {
     public Guid Id { get; set; }
+    public required string Nickname { get; set; }
+    public Guid? CityId { get; set; }
     
     public required string IdentityId { get; set; }
     
