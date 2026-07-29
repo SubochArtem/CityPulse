aa82424c8e4b073c9e70b5cdde92c3877fe6f828
Artem <ghtp7397@gmail.com>
2026-07-29 11:45:14 +0300

    feat(business): add ValidationConstants in CreateUserValidator

--- src/Backend/Services/Users/Users.Business/Validators/CreateUserValidator.cs
+++ src/Backend/Services/Users/Users.Business/Validators/CreateUserValidator.cs
@@ -6,15 +6,12 @@ namespace Users.Business.Validators;
 
 public class CreateUserValidator : AbstractValidator<CreateUserDto>
 {
-    private const string IdentityIdPattern =
-        @"^[a-zA-Z0-9_-]+\|[a-zA-Z0-9@._-]+$";
-
     public CreateUserValidator()
     {
         RuleFor(x => x.IdentityId)
             .NotEmpty()
-            .WithMessage(ValidationMessages.IdentityIdRequired)
-            .Matches(IdentityIdPattern)
-            .WithMessage(ValidationMessages.IdentityIdInvalidFormat);
+            .WithMessage(ValidationConstants.IdentityId.Required)
+            .Matches(ValidationConstants.IdentityId.Pattern)
+            .WithMessage(ValidationConstants.IdentityId.InvalidFormat);
     }
 }
